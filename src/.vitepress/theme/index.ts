47ae7f3a350d07e2fbedc5e3332c2fd8f449a184
Paul Laros <paul@laros.io>
2023-04-05 00:13:56 +0200

    Add @vercel/analytics

--- src/.vitepress/theme/index.ts
+++ src/.vitepress/theme/index.ts
@@ -1,6 +1,9 @@
+import { inject } from '@vercel/analytics'
 import DefaultTheme from 'vitepress/theme'
 
 import './custom.css'
 import './tailwind.postcss'
+
+inject();
 
 export default DefaultTheme
