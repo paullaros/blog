7d0b448da9b7ce0d4079ddd6bd361f5bf47bc5de
Paul Laros <paul@laros.io>
2023-04-07 01:15:25 +0200

    Mobile styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,4 +3,13 @@ module.exports = {
 		'./.vitepress/**/*.{js,ts,vue}',
 		'./src/**/*.md',
 	],
+  theme: {
+    // Breakpoints based on https://github.com/vuejs/vitepress/tree/main/src/client/theme-default
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '960px',
+      'xl': '1280px'
+    }
+  }
 }
