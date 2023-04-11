8901fe3cb50032c60e38ad8f00cf72191975713a
Paul Laros <paul@laros.io>
2023-04-11 18:42:09 +0200

    Add Date component

---
+++ .vitepress/theme/components/Date.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { ref, computed, watchEffect, onMounted } from 'vue'
+import { useData } from '../composables/data'
+
+const { page, lang } = useData()
+
+const date = computed(() => new Date(page.value.frontmatter.date!))
+const isoDatetime = computed(() => date.value.toISOString())
+const datetime = ref('')
+
+// set time on mounted hook to avoid hydration mismatch due to
+// potential differences in timezones of the server and clients
+onMounted(() => {
+  watchEffect(() => {
+    datetime.value = date.value.toLocaleDateString(lang.value, {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    })
+  })
+})
+</script>
+
+<template>
+  <p class="VPDate" v-if="page.frontmatter.date">
+    <time :datetime="isoDatetime">{{ datetime }}</time>
+  </p>
+</template>
+
+<style scoped>
+.VPDate {
+  line-height: 24px;
+  font-size: 14px;
+  font-weight: 500;
+  color: var(--vp-c-text-2);
+}
+
+@media (min-width: 640px) {
+  .VPDate {
+    line-height: 32px;
+    font-size: 14px;
+    font-weight: 500;
+  }
+}
+</style>
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,9 +1,20 @@
 import { inject } from '@vercel/analytics'
+import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
+
+// @ts-ignore
+import Date from './components/Date.vue'
 
 import './tailwind.css'
 import './custom.css'
 
 inject();
 
-export default DefaultTheme
+export default {
+  ...DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'doc-before': () => h(Date)
+    })
+  }
+}
