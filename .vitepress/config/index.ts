8e35ec8dd9dfa20af45f7c2397d003701c37dd9c
Paul Laros <paul@laros.io>
2024-09-27 16:52:06 +0200

    Migrate sitemap generation

--- .vitepress/config/index.ts
+++ .vitepress/config/index.ts
@@ -4,7 +4,6 @@ import theme from "./theme"
 import head from "./head"
 
 import generateMeta from './hooks/generateMeta'
-import generateSitemap from './hooks/generateSitemap'
 import generateFeed from './hooks/generateFeed'
 
 const hostname: string = 'https://laros.io'
@@ -17,13 +16,15 @@ export default defineConfig({
   lang: 'en-US',
   title: 'Paul Laros',
   description: 'Developer with a passion for writing clean and functional code, and bringing beautiful ideas to life',
+  sitemap: {
+    hostname: 'https://laros.io'
+  },
   head,
   themeConfig: theme,
   transformHead: async (context) => (
     generateMeta(context, hostname)
   ),
   buildEnd: async (context) => {
-    generateSitemap(context, hostname)
     generateFeed(context, hostname)
   }
 })
